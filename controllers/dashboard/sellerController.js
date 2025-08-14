bc31bda7a1e330b3b72fd2dd612487b76a9a3714
kasim <student123kasim@gmail.com>
2025-08-14 23:35:41 +0530

    chnage

---
+++ controllers/dashboard/sellerController.js
@@ -0,0 +1,36 @@
+const { responseReturn } = require("../../utiles/response");
+const cloudinary = require('cloudinary').v2
+const sellerModel = require('../../models/sellerModel')
+const formidabel = require('formidable');
+
+class selleryController { 
+  
+    request_seller_get = async (req, res) => {
+
+        const {page, searchValue, parPage} = req.query;
+        const skipPage = parseInt(parPage) * (parseInt(page) - 1)
+
+         try {
+
+            if(searchValue){
+
+            }else{
+               const sellers = await sellerModel.find({status: 'pending'}).skip(skipPage).limit(parPage).sort({ createdAt : -1 })
+               const totalSeller = await sellerModel.find({ status: 'pending'}).countDocuments()
+                responseReturn(res, 200, {sellers, totalSeller})
+           
+            }
+            
+         } catch (error) {
+            responseReturn(res, 500, {error:error.message})
+         }
+         
+       
+
+
+    }
+
+
+}
+
+module.exports = new selleryController();
---
+++ routes/dashboard/sellerRoutes.js
@@ -0,0 +1,8 @@
+
+const sellerController = require('../../controllers/dashboard/sellerController');
+const { authMiddleware } = require('../../middlewares/authMiddleware');
+const router = require('express').Router();
+
+router.get('/request-seller-get', authMiddleware, sellerController.request_seller_get);
+
+module.exports = router;
